a2537185f0514c39514601bd3c2e01921cdbf258
Chuck Rakaczky <cjr105@case.edu>
2019-10-25 20:32:31 -0400

    Finished RateInstructor Component, just the frontend, and added to HelpImproveSearch.

--- website/website/src/components/HelpImproveSearch.js
+++ website/website/src/components/HelpImproveSearch.js
@@ -1,6 +1,6 @@
 import React, { Component} from 'react';
 import { Form, Input, ListGroup, ListGroupItem, CustomInput, FormGroup, Label, Modal, ModalHeader, ModalBody, ModalFooter, Table, Button, Collapse, CardBody, Card, UncontrolledCollapse, FormText, Col, ButtonGroup } from 'reactstrap';
-
+import RateInstructor from './RateInstructor';
 
 export default class HelpDesk extends Component {
     state = {
@@ -38,98 +38,19 @@ export default class HelpDesk extends Component {
                             </ListGroupItem>
                             <ListGroupItem tag="button" action>Type your syllabus</ListGroupItem>
                             <ListGroupItem tag="button" action>Rate A Class</ListGroupItem>
-                            <ListGroupItem tag="button" id="rateInstructorToggler" action>Rate An Instructor</ListGroupItem>
+                            <ListGroupItem tag="button" id="rateInstructor" action>Rate An Instructor</ListGroupItem>
+                            <RateInstructor/>
 
-                            <UncontrolledCollapse toggler="#rateInstructorToggler">
-                              <Card>
-                                <CardBody>
-                                <Form>
-                                    <FormGroup row>
-                                      <Label for="professorNameFirst">Professor Name</Label>
-                                    </FormGroup>
-                                    <FormGroup row>
-                                      <Col sm={50}>
-                                          <Input type="text" name="professorNameFirst" id="professorNameFirst" placeholder="First" />
-                                      </Col>
-                                      <Col sm={50}>
-                                          <Input type="text" name="professorNameLast" id="professorNameLast" placeholder="Last" />
-                                      </Col>
-                                    </FormGroup>
-
-                                    <FormGroup row>
-                                      <Label for="courseCode">Course Code</Label>
-                                    </FormGroup>
-                                    <FormGroup row>
-                                      <Input type="text" name="courseCode" id="courseCode" placeholder="ex. EECS132" />
-                                    </FormGroup>
-
-                                    <FormGroup row>
-                                      <Label for="personalityRating">Personality Rating</Label>
-                                    </FormGroup>
-                                    <FormGroup row>
-                                      <ButtonGroup>
-                                        <Button>1</Button>
-                                        <Button>2</Button>
-                                        <Button>3</Button>
-                                        <Button>4</Button>
-                                        <Button>5</Button>
-                                        <Button>6</Button>
-                                        <Button>7</Button>
-                                        <Button>8</Button>
-                                        <Button>9</Button>
-                                        <Button>10</Button>
-                                      </ButtonGroup>
-                                    </FormGroup>
-
-                                    <FormGroup row>
-                                      <Label for="teachingQualityRating">Teaching Quality Rating</Label>
-                                    </FormGroup>
-                                    <FormGroup row>
-                                      <ButtonGroup>
-                                        <Button>1</Button>
-                                        <Button>2</Button>
-                                        <Button>3</Button>
-                                        <Button>4</Button>
-                                        <Button>5</Button>
-                                        <Button>6</Button>
-                                        <Button>7</Button>
-                                        <Button>8</Button>
-                                        <Button>9</Button>
-                                        <Button>10</Button>
-                                      </ButtonGroup>
-                                    </FormGroup>
-
-                                    <FormGroup row>
-                                      <Label for="difficultyRating">Difficulty Rating (1 = Very Easy, 10 = Very Hard)</Label>
-                                    </FormGroup>
-                                    <FormGroup row>
-                                      <ButtonGroup>
-                                        <Button>1</Button>
-                                        <Button>2</Button>
-                                        <Button>3</Button>
-                                        <Button>4</Button>
-                                        <Button>5</Button>
-                                        <Button>6</Button>
-                                        <Button>7</Button>
-                                        <Button>8</Button>
-                                        <Button>9</Button>
-                                        <Button>10</Button>
-                                      </ButtonGroup>
-                                    </FormGroup>
-
-                                  </Form>
-                                </CardBody>
-                              </Card>
-                            </UncontrolledCollapse>
                         </ListGroup>
 
                     </ModalBody>
                     <ModalFooter>
-                        {/* <Button color="primary" onClick={this.routeToSearchPage.bind(this)}>Search</Button>{' '} */}
                         <Button color="secondary" onClick={this.toggleHelpDesk.bind(this)}>Cancel</Button>
                     </ModalFooter>
                 </Modal>
             </div>
+
         )
     }
+
 }
