21e58349c98bd57b940578172f41ccab09c2ba22
justinphan3110 <justinphan3110@gmail.com>
2019-10-29 13:40:50 -0400

    minor fix

--- crawling/sis/SISData.py
+++ crawling/sis/SISData.py
@@ -96,9 +96,9 @@ class SISData:
 
 cleaner = SISData()
 # cleaner.reading(r'D:\ClassSearchEngine\crawling\sis\soc.xml')
-# cleaner.reading('/home/long/github/ClassSearchEngine/lib/soc.xml')
-cleaner.reading('/home/lnp26/github/ClassSearchEngine/lib/soc_2018.xml')
 cleaner.reading('/home/lnp26/github/ClassSearchEngine/lib/soc.xml')
+cleaner.reading('/home/lnp26/github/ClassSearchEngine/lib/soc_2018.xml')
+# cleaner.reading('/home/lnp26/github/ClassSearchEngine/lib/soc.xml')
 
 
 
